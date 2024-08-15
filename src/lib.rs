1404b6e51a6990e67be079f21107f2adb1f2d205
Stanislav Stoyanov <grimerssy@gmail.com>
2024-08-15 20:34:27 +0200

    fix path to proptest crate

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,17 @@ macro_rules! prop_test {
     ($strategy:expr, $test:expr, $config:expr) => {
         let mut config = $config;
         config.source_file = Some(file!());
-        let mut runner = ::proptest::test_runner::TestRunner::new(config);
+        let mut runner = $crate::proptest::test_runner::TestRunner::new(config);
         match runner.run($strategy, $test) {
             Ok(()) => (),
             Err(e) => panic!("{e}\n{runner}"),
         }
     };
     ($strategy:expr, $test:expr) => {
-        $crate::prop_test!($strategy, $test, ::proptest::test_runner::Config::default())
+        $crate::prop_test!(
+            $strategy,
+            $test,
+            $crate::proptest::test_runner::Config::default()
+        )
     };
 }
